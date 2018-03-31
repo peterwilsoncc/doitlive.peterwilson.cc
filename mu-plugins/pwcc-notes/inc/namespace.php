f8ff2a435655df309cef8bce12623cf2ada2fb76
Peter Wilson <git-commit-by@peterwilson.cc>
2018-03-31 19:16:16 +1100

    Register Twitter fields for HM-CMB.

--- mu-plugins/pwcc-notes/inc/namespace.php
+++ mu-plugins/pwcc-notes/inc/namespace.php
@@ -56,5 +56,68 @@ function register_cpt() {
  * @return array Modified meta boxes.
  */
 function register_meta_boxes( array $meta_boxes ) {
+	$twitter_fields = [
+		[
+			'id'    => 'text',
+			'name'  => 'Text',
+			'type'  => 'textarea',
+			'rows'  => '3',
+			'class' => 'pwccindieweb-note-text',
+		],
+		[
+			'id'      => 'post_on_twitter',
+			'name'    => 'Post on twitter',
+			'type'    => 'radio',
+			'options' => [
+				'1'  => 'Yes',
+				'no' => 'No',
+			],
+			'cols'    => '6',
+			'default' => '1',
+			'class'   => 'pwccindieweb-note-post-on-twitter',
+		],
+		[
+			'id'      => 'append_url',
+			'name'    => 'Append URL to post',
+			'type'    => 'radio',
+			'options' => [
+				'1'  => 'Yes',
+				'no' => 'No',
+			],
+			'default' => 'no',
+			'cols'    => '6',
+			'class'   => 'pwccindieweb-note-append-url',
+		],
+		[
+			'id'             => 'images',
+			'name'           => 'Images to include',
+			'type'           => 'image',
+			'class'          => 'pwccindieweb-note-images',
+			'repeatable'     => true,
+			'repeatable_max' => 4,
+		],
+	];
+
+	$fields = [
+		[
+			'id'         => '_pwccindieweb-note',
+			'desc'       => 'Post to Twitter',
+			'name'       => 'Twitter',
+			'type'       => 'group',
+			'class'      => 'pwccindieweb-twitter',
+			'repeatable' => false,
+			'fields'     => $twitter_fields,
+		],
+	];
+
+	$meta_boxes[] = [
+		'id'       => 'pwccindieweb_notes_metabox',
+		'title'    => 'Notes',
+		'pages'    => [ 'post', 'page', 'pwcc_notes' ],
+		'context'  => 'normal',
+		'priority' => 'high',
+		'fields'   => $fields
+	];
+
 	return $meta_boxes;
 }
