5e04327bb631d0db205b8dfa389606e832de6867
Peter Wilson <git-commit-by@peterwilson.cc>
2018-03-31 16:47:58 +1100

    Shell of Notes plugin.

---
+++ mu-plugins/pwcc-notes/inc/namespace.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * PWCC Notes.
+ *
+ * @package     PWCCNotes
+ * @author      Peter Wilson
+ * @copyright   2018 Peter Wilson
+ * @license     GPL-2.0+
+ */
+
+namespace PWCC\Notes;
+/**
+ * Bootstrap notes.
+ */
+function bootstrap() {
+}
