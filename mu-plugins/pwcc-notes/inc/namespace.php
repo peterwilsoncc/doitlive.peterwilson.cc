e59c52fe93c8566f2a19a574acd8da67a6c53d42
Peter Wilson <git-commit-by@peterwilson.cc>
2018-03-31 18:02:55 +1100

    Register custom fields for notes.

--- mu-plugins/pwcc-notes/inc/namespace.php
+++ mu-plugins/pwcc-notes/inc/namespace.php
@@ -21,6 +21,7 @@ function bootstrap() {
 		exit;
 	}
 	add_action( 'init', __NAMESPACE__ . '\\register_cpt' );
+	add_action( 'cmb2_admin_init', __NAMESPACE__ . '\\register_meta_boxes' );
 }
 
 /**
@@ -42,6 +43,80 @@ function register_cpt() {
 			'singular' => 'Note',
 			'plural'   => 'Notes',
 			'slug'     => '~',
+		]
+	);
+}
+
+/**
+ * Register meta boxes using CMB2.
+ *
+ * Runs on the `cmb2_admin_init` hook.
+ */
+function register_meta_boxes() {
+	$cmb = new_cmb2_box( [
+		'id'           => 'pwccindieweb_notes_metabox',
+		'title'        => __( 'Notes', 'pwcc' ),
+		'object_types' => [ 'pwcc_notes', 'post', 'page' ],
+		'context'      => 'normal',
+		'priority'     => 'high',
+	] );
+
+	$group = $cmb->add_field( [
+		'name'       => esc_html__( 'Twitter', 'pwcc' ),
+		'desc'       => esc_html__( 'Post to Twitter', 'pwcc' ),
+		'id'         => '_pwccindieweb-note',
+		'type'       => 'group',
+		'classes'    => 'pwccindieweb-twitter',
+		'repeatable' => false,
+	] );
+
+	$cmb->add_group_field(
+		$group,
+		[
+			'name' => __( 'Text', 'pwcc' ),
+			'id' => 'text',
+			'type' => 'textarea_small',
+			'classes' => 'pwccindieweb-note-text',
+		]
+	);
+
+	$cmb->add_group_field(
+		$group,
+		[
+			'name'    => __( 'Post on twitter', 'pwcc' ),
+			'id'      => 'post_on_twitter',
+			'type'    => 'radio_inline',
+			'options' => [
+				'1'  => esc_html__( 'Yes', 'pwcc' ),
+				'no' => esc_html__( 'No', 'pwcc' ),
+			],
+			'default' => 'no',
+			'classes' => 'pwccindieweb-note-post-on-twitter',
+		]
+	);
+
+	$cmb->add_group_field(
+		$group,
+		[
+			'name'    => __( 'Append URL to post', 'pwcc' ),
+			'id'      => 'append_url',
+			'type'    => 'radio_inline',
+			'options' => [
+				'1'  => esc_html__( 'Yes', 'pwcc' ),
+				'no' => esc_html__( 'No', 'pwcc' ),
+			],
+			'default' => 'no',
+			'classes' => 'pwccindieweb-note-append-url',
+		]
+	);
+
+	$cmb->add_group_field(
+		$group,
+		[
+			'name'    => __( 'Images to include', 'pwcc' ),
+			'id'      => 'images',
+			'type'    => 'file_list',
+			'classes' => 'pwccindieweb-note-images',
 		]
 	);
 }
