a0e62905e98629c686522b2aacb3085beb696fc8
Peter Wilson <git-commit-by@peterwilson.cc>
2018-03-31 17:12:17 +1100

    Register the CPT.

--- mu-plugins/pwcc-notes/inc/namespace.php
+++ mu-plugins/pwcc-notes/inc/namespace.php
@@ -12,6 +12,36 @@ namespace PWCC\Notes;
 
 /**
  * Bootstrap notes.
+ *
+ * Runs on the `plugins_loaded` hook.
  */
 function bootstrap() {
+	if ( ! function_exists( 'register_extended_post_type' ) ) {
+		// Extended Post Types plugin is not enabled. Panic.
+		exit;
+	}
+	add_action( 'init', __NAMESPACE__ . '\\register_cpt' );
+}
+
+/**
+ * Register Note CPT.
+ *
+ * Runs on the `init` hook.
+ */
+function register_cpt() {
+	register_extended_post_type(
+		'pwcc_notes',
+		[
+			'description'     => 'Short, Twitter like, notes.',
+			'capability_type' => 'post',
+			'hierarchical'    => false,
+			'supports'        => [ 'comments', 'trackbacks', 'author', 'editor', 'title', 'post-formats' ],
+			'has_archive'     => true,
+		],
+		[
+			'singular' => 'Note',
+			'plural'   => 'Notes',
+			'slug'     => '~',
+		]
+	);
 }
