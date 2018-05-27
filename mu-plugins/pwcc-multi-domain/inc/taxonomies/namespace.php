f6b26f31a7abd64b1c051e7b989ff661545c7018
Peter Wilson <git-commit-by@peterwilson.cc>
2018-05-27 12:33:22 +1000

    Filter term links.

--- mu-plugins/pwcc-multi-domain/inc/taxonomies/namespace.php
+++ mu-plugins/pwcc-multi-domain/inc/taxonomies/namespace.php
@@ -17,4 +17,75 @@ use WP_Error;
  * Runs on the `plugins_loaded` filter.
  */
 function bootstrap() {
+	add_filter( 'term_link', __NAMESPACE__ . '\\filter_term_link', 10, 3 );
+}
+
+/**
+ * Array of custom domains used for taxonomies.
+ *
+ * @return array Taxonomy => custom home URL key/value pairs.
+ */
+function custom_home_urls() {
+	$custom_homes = [
+		'DEFAULT' => 'http://peterwilsoncc.local',
+	];
+
+	/**
+	 * Filters the custom home URLs used for post types.
+	 *
+	 * @param array Taxonomy => custom home URL key/value pairs.
+	 */
+	$custom_homes = apply_filters( 'pwcc/multi-domain/taxonomies/domains', $custom_homes );
+
+	$custom_homes = array_map( function( $value ) {
+		return esc_url_raw( trailingslashit( $value ) );
+	}, $custom_homes );
+
+	return $custom_homes;
+}
+
+/**
+ * Returns the preferred home URL for a taxonomy.
+ *
+ * @param string $taxonomy The taxonomy.
+ * @return string|WP_Error The home URL. WP_Error if unable to determine.
+ */
+function get_taxos_custom_home( string $taxonomy ) {
+	$custom_home_urls = custom_home_urls();
+
+	if ( isset( $custom_home_urls[ $taxonomy ] ) ) {
+		return $custom_home_urls[ $taxonomy ];
+	}
+
+	if ( isset( $custom_home_urls['DEFAULT'] ) ) {
+		return $custom_home_urls['DEFAULT'];
+	}
+
+	if ( ! doing_filter( 'home_url' ) ) {
+		return get_home_url();
+	}
+
+	return new WP_Error(
+		'pwcc/multi-domain/taxonomies/unable-to-determine-domain',
+		'Unable to determine domain to use for Taxonomy'
+	);
+}
+
+/**
+ * Filter term permalink to preferred use preferred home URL.
+ *
+ * Runs on the following filters:
+ * - term_link
+ *
+ * @param string $termlink Term link URL.
+ * @param \WP_Term $term     Term object.
+ * @param string $taxonomy Taxonomy slug.
+ */
+function filter_term_link( string $termlink, $term, $taxonomy ) {
+	$termlink_home = get_taxos_custom_home( $taxonomy );
+	if ( is_wp_error( $termlink_home ) ) {
+		return $termlink;
+	}
+
+	return MultiDomain\normalise_url( $termlink, $termlink_home );
 }
