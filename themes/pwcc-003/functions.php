4d55ba9172a12d683a55d937a21f3c4b1af92183
Peter Wilson <git-commit-by@peterwilson.cc>
2018-06-30 18:23:20 +1000

    Move things to a namespace.

--- themes/pwcc-003/functions.php
+++ themes/pwcc-003/functions.php
@@ -1,35 +1,4 @@
 <?php
 namespace PWCC\Theme;
 
-use HumanMade\AssetLoader;
-
-function bootstrap() {
-	add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\\enqueue' );
-}
 add_action( 'after_setup_theme', __NAMESPACE__ . '\\bootstrap' );
-
-function enqueue() {
-	$loaded = AssetLoader\enqueue_assets(
-		__DIR__ . '/assets', [
-			'handle'  => 'pwcc-003-app',
-			'scripts' => [],
-		]
-	);
-
-	if ( ! $loaded ) {
-		wp_enqueue_script(
-			'pwcc-003-app',
-			get_stylesheet_directory_uri() . '/assets/build/app.js',
-			[],
-			'1.0.0-alpha',
-			true
-		);
-
-		wp_enqueue_style(
-			'pwcc-003-app',
-			get_stylesheet_directory_uri() . '/assets/build/app.css',
-			[],
-			'1.0.0-alpha'
-		);
-	}
-}
