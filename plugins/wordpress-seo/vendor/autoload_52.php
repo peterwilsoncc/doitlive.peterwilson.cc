f65ece71121c322b5be8ddea9897c1935fd1e0e7
Peter Wilson <git-commit-by@peterwilson.cc>
2018-08-04 12:29:17 +0100

    Bump Yoast SEO to 7.9

--- plugins/wordpress-seo/vendor/autoload_52.php
+++ plugins/wordpress-seo/vendor/autoload_52.php
@@ -4,4 +4,4 @@
 
 require_once dirname(__FILE__) . '/composer'.'/autoload_real_52.php';
 
-return ComposerAutoloaderInit9922ad94b67f7fe50c9ff4b2c1a53952::getLoader();
+return ComposerAutoloaderInit6ac2ab3c6f1ba886ca08991693c194ec::getLoader();
