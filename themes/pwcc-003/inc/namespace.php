02aabd5bc5a06d816fdecab7290ac46d1d67135e
Peter Wilson <git-commit-by@peterwilson.cc>
2018-12-23 17:50:18 +1100

    Configure theme support.

--- themes/pwcc-003/inc/namespace.php
+++ themes/pwcc-003/inc/namespace.php
@@ -4,6 +4,7 @@ namespace PWCC\Theme;
 use HumanMade\AssetLoader;
 
 function bootstrap() {
+	theme_setup();
 	add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\\enqueue' );
 }
 
@@ -25,6 +26,48 @@ function get_theme_version() {
 	$version = $theme_data->get( 'Version' ) === '%%VERSION%%' ? microtime( true ) : $theme_data->get( 'Version' );
 
 	return $version;
+}
+
+/**
+ * Set up theme defaults and supported features within WordPress.
+ *
+ * Runs on the action `after_setup_theme`.
+ */
+function theme_setup() {
+	// Add default posts and comments RSS feed links to head.
+	add_theme_support( 'automatic-feed-links' );
+
+	/*
+	 * Let WordPress manage the document title.
+	 * By adding theme support, we declare that this theme does not use a
+	 * hard-coded <title> tag in the document head, and expect WordPress to
+	 * provide it for us.
+	 */
+	add_theme_support( 'title-tag' );
+
+	/*
+	 * Switch default core markup for search form, comment form, and comments
+	 * to output valid HTML5.
+	 */
+	add_theme_support(
+		'html5',
+		array(
+			'search-form',
+			'comment-form',
+			'comment-list',
+			'gallery',
+			'caption',
+		)
+	);
+
+	// Add support for Block Styles.
+	add_theme_support( 'wp-block-styles' );
+
+	// Add support for full and wide align images.
+	add_theme_support( 'align-wide' );
+
+	// Add support for responsive embedded content.
+	add_theme_support( 'responsive-embeds' );
 }
 
 /**
