3ae444fcfb0d83f5204f473a19ea8ee516f1e86e
Peter Wilson <git-commit-by@peterwilson.cc>
2018-12-26 15:40:02 +1100

    A legal bar for the footer.

--- themes/pwcc-003/footer.php
+++ themes/pwcc-003/footer.php
@@ -1,3 +1,26 @@
+<?php
+use PWCC\Theme;
+?>
+<footer class="footer">
+	<div class="footer__legal legal">
+		<p class="legal__copyright">
+			&copy; Peter Wilson <?php Theme\the_copyright_dates(); ?>
+		</p>
+		<?php
+		wp_nav_menu( [
+			'theme_location' => 'legal-nav',
+			'container' => 'nav',
+			'container_id' => null,
+			'container_class' => 'legal__nav legal-nav',
+			'menu_id' => 'global-nav__menu',
+			'menu_class' => 'global-nav__menu',
+			'fallback_cb' => false,
+			'depth' => 1,
+			'item_spacing' => 'discard',
+		] );
+		?>
+	</div>
+</footer>
 <?php wp_footer(); ?>
 </body>
 </html>
