24544ea94c723d329d919e06a60ddcd8eb550a04
Peter Wilson <519727+peterwilsoncc@users.noreply.github.com>
2020-10-19 09:08:33 +1100

    Bump Plugins.

--- plugins/jetpack/_inc/build/admin.asset.php
+++ plugins/jetpack/_inc/build/admin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '5c94a36fe21e9c9d901cdc0619ca59d6');
+<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '12ede2807225d7390a647b6efd055f75');
