6f88a4b8f214283bbdfba87e8ba3938332b0af08
Peter Wilson <519727+peterwilsoncc@users.noreply.github.com>
2022-07-27 13:34:30 +1000

    Update all the plugins.

--- plugins/vaultpress/vendor/jetpack-autoloader/class-version-selector.php
+++ plugins/vaultpress/vendor/jetpack-autoloader/class-version-selector.php
@@ -5,7 +5,7 @@
  * @package automattic/jetpack-autoloader
  */
 
-namespace Automattic\Jetpack\Autoloader\jp9559eef123208b7d1b9c15b978567267_vaultpressⓥ2_2_1;
+namespace Automattic\Jetpack\Autoloader\jp9559eef123208b7d1b9c15b978567267_vaultpressⓥ2_2_2;
 
  // phpcs:ignore
 
@@ -29,7 +29,7 @@ class Version_Selector {
 	public function is_version_update_required( $selected_version, $compare_version ) {
 		$use_dev_versions = defined( 'JETPACK_AUTOLOAD_DEV' ) && JETPACK_AUTOLOAD_DEV;
 
-		if ( is_null( $selected_version ) ) {
+		if ( $selected_version === null ) {
 			return true;
 		}
 
--- plugins/wordpress-seo/vendor/autoload.php
+++ plugins/wordpress-seo/vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInit3f991ead80bed1d5dc3fc5b5044f3566::getLoader();
+return ComposerAutoloaderInite59fe193b20dcf415ae6f86cb6637c47::getLoader();
