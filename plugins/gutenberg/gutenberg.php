c5b5cecf8319407df2412242dac58b197f9e44d8
Peter Wilson <519727+peterwilsoncc@users.noreply.github.com>
2023-04-17 10:06:51 +1000

    Update all the things.

--- plugins/gutenberg/gutenberg.php
+++ plugins/gutenberg/gutenberg.php
@@ -5,7 +5,7 @@
  * Description: Printing since 1440. This is the development plugin for the block editor, site editor, and other future WordPress core functionality.
  * Requires at least: 6.0
  * Requires PHP: 5.6
- * Version: 15.4.0
+ * Version: 15.5.1
  * Author: Gutenberg Team
  * Text Domain: gutenberg
  *
@@ -13,8 +13,8 @@
  */
 
 ### BEGIN AUTO-GENERATED DEFINES
-define( 'GUTENBERG_VERSION', '15.4.0' );
-define( 'GUTENBERG_GIT_COMMIT', 'd06a74d71fdd722b2ee037133867134ce956f44e' );
+define( 'GUTENBERG_VERSION', '15.5.1' );
+define( 'GUTENBERG_GIT_COMMIT', '87969ad818d0821f0bc40c864cde6d31e18a6283' );
 ### END AUTO-GENERATED DEFINES
 
 gutenberg_pre_init();
