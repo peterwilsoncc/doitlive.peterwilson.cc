7566885f9de83963992748b102da7747fc88102b
Peter Wilson <519727+peterwilsoncc@users.noreply.github.com>
2024-06-27 13:40:49 +1000

    Delete unused plugins.

--- plugins/jetpack-boost/app/modules/image-guide/dist/guide.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wp-i18n', 'wp-polyfill'), 'version' => '32ee7a08ac970bdd1e1f');
--- plugins/jetpack-boost/jetpack_vendor/automattic/jetpack-plugin-deactivation/build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wp-polyfill'), 'version' => '655c3ff32e3192c47697');
